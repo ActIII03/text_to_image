672fd7e484668890f4fea02eee4a25c4452ba99d
ActIII03 <a.touche03@gmail.com>
2022-12-04 20:04:35 -0800

    made a gcloud sdk script for IAM mgmt

---
+++ final/get_cred.sh
@@ -0,0 +1,24 @@
+# Script to create a new google cloud role and generate a key (json file)
+# Assumes you have gcloud sdk installed and configured
+
+# Set the project name
+PROJECT_NAME="flask-ai-py-app"
+
+# Set the role name
+ROLE_NAME="textimage-pyapp"
+
+# Create the role w/ datastore access (roles/datastore.user), storage access (roles/storage.objectAdmin)
+# gcloud iam service-accounts create $ROLE_NAME --display-name $ROLE_NAME --project $PROJECT_NAME
+
+# Create a key for the role
+# gcloud iam service-accounts keys create $ROLE_NAME.json --iam-account $ROLE_NAME@$PROJECT_NAME.iam.gserviceaccount.com
+# 
+# # Grant the role access to the project
+# gcloud projects add-iam-policy-binding $PROJECT_NAME --member serviceAccount:$ROLE_NAME@$PROJECT_NAME.iam.gserviceaccount.com --role roles/datastore.user --role roles/storage.objectAdmin
+# 
+# # Download the key as a json file
+# gcloud iam service-accounts keys create $ROLE_NAME.json --iam-account $ROLE_NAME@$PROJECT_NAME.iam.gserviceaccount.com
+
+# # Set the environment variable
+export GOOGLE_APPLICATION_CREDENTIALS=$PWD/$ROLE_NAME.json
+# 
