01d01d6b0c7fa8df53199a87736b64efcdfccd11
ActIII03 <a.touche03@gmail.com>
2022-12-07 16:15:02 -0800

    re-added get model

--- final/index.py
+++ final/index.py
@@ -4,7 +4,6 @@ import gbmodel
 
 class Index(MethodView):
   def get(self):
-    # model = gbmodel.get_model()
-    # ai_images = model.select()
-    # return render_template("index.html", ai_images=ai_images)
-    return render_template("index.html")
+    model = gbmodel.get_model()
+    ai_images = model.select()
+    return render_template("index.html", ai_images=ai_images)
